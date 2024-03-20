4ca2f04019827442e47480a086105069a29a1cb3
samblooms <16036598+samblooms@users.noreply.github.com>
2024-03-19 20:12:41 -0500

    cleanup

--- tests/data_initialization/test_download_data_files.py
+++ tests/data_initialization/test_download_data_files.py
@@ -6,11 +6,13 @@ import config
 from nera.data_initialization.parse_urls_from_json import parse_urls_from_json
 from nera.data_initialization.download_data_files import download_files
 
+
 class TestDownloadDataFiles:
 
 
     @pytest.fixture
     def setup_data(self):
+        
         # Setup data directory and file paths
         contractor_json_file = config.CONTRACTOR_JSON_FILES_DIR / 'all_6xx_Jun_29.json'
         download_dir = config.TEMP_TRAINING_DIR
@@ -20,10 +22,8 @@ class TestDownloadDataFiles:
     def test_download_files(self, setup_data):
 
         contractor_json_file, download_dir = setup_data
-
         urls = parse_urls_from_json(contractor_json_file)
         download_files(urls, download_dir) 
-
         downloaded_files = os.listdir(download_dir) # Get a list of all files in the download directory
         url_filenames = [os.path.basename(url) for url in urls]  # Get the filenames of the URLs (i.e the last part of the URL, like https://www.example.com/file.mp4 -> file.mp4)
 
--- tests/data_initialization/test_parse_urls_from_json.py
+++ tests/data_initialization/test_parse_urls_from_json.py
@@ -1,25 +1,31 @@
 import pytest
 import config
 
-
 from nera.data_initialization.parse_urls_from_json import parse_urls_from_json
+
 
 class TestParseUrlsFromJson:
 
 
     @pytest.fixture
     def setup_data(self):
+
         # Setup data directory and file paths
         contractor_json_file = config.CONTRACTOR_JSON_FILES_DIR / 'all_6xx_Jun_29.json'
         return contractor_json_file
 
 
-    def test_parse_urls_from_json(self, setup_data):
-
-        contractor_json_file = setup_data
-        urls = parse_urls_from_json(contractor_json_file) # Parse URLs from the JSON file
-        
+    def test_parse_urls_returns_list(self, setup_data):
+        urls = parse_urls_from_json(setup_data)
         assert isinstance(urls, list), "URLs should be in a list"
+
+
+    def test_parse_urls_list_not_empty(self, setup_data):
+        urls = parse_urls_from_json(setup_data)
         assert len(urls) > 0, "URLs list should not be empty"
+
+
+    def test_parse_urls_list_contains_strings(self, setup_data):
+        urls = parse_urls_from_json(setup_data)
         assert all(isinstance(url, str) for url in urls), "All elements in the list should be strings"
             
