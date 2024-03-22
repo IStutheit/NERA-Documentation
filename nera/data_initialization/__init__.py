7edbe6d21ad34ae16a8588c094e2d906330339b9
IStutheit <ImAgonizzy@gmail.com>
2024-03-21 19:16:28 -0500

    Added for the purpose a Pdoc Landing Page

---
+++ nera/data_initialization/__init__.py
@@ -0,0 +1,17 @@
+"""
+#The Data Initalization Section contains all files for processing the training data from OpenAI.
+
+---
+
+## **[Download Data Files](http://localhost:8080/CSCI4970-MC-GamePlayingBot/nera/data_initialization/download_data_files.html)**
+> Grabs the urls from the contractor data to be downloaded and stored.
+
+## **[Clear Training Data](http://localhost:8080/CSCI4970-MC-GamePlayingBot/nera/data_initialization/clear_training_data.html)**
+> Deletes all files in the specified directory.
+
+## **[Process Video Frames](http://localhost:8080/CSCI4970-MC-GamePlayingBot/nera/data_initialization/process_video_frames.html)**
+> Takes in multiple files in parallel and transforms the frames into grayscale images.
+
+
+
+"""
