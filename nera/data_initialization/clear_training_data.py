c47c18b020c2bbcdd5e0f57d8805098d492ff150
Sam Bloomingdale <16036598+samblooms@users.noreply.github.com>
2024-04-06 17:23:21 -0500

    update to only removed files contained within NERA_TRAINING_DATA_INDEX_FILE for safety

--- nera/data_initialization/clear_training_data.py
+++ nera/data_initialization/clear_training_data.py
@@ -1,23 +1,56 @@
 import os
+import json
+
+from nera.utils.output_helpers import HORIZONTAL_LINE
 
 
 def clear_training_data(data_dir):
     """
-    Deletes all files in the specified directory.
-    Gotta be careful with this one!
+    Recursively search through the directory to delete all training data files.
 
+    We use this to avoid using wildcards or other methods that could potentially delete files we don't want to delete.
+    In every directory, and subdirectory, check for a "NERA_TERAINING_DATA_INDEX_FILE.json" file.
+    If it exists, delete all the files listed in the file.
+    
     Args:
-        data_dir (str): The path to the directory to be cleared.
+    - data_dir (str): The directory to search for the "NERA_TERAINING_DATA_INDEX_FILE.json" files.
+    Returns:
+    - deleted_files (list of str): A list of all the files that were deleted.
     """
-    # List all files in the folder
-    files = os.listdir(data_dir)
+    
+    # Store a list of all the files that were deleted
+    deleted_files = []
+    errors = []
 
-    # Loop through the files and delete them
-    for file in files:
-        file_path = os.path.join(data_dir, file)
-        if os.path.isfile(file_path):  # Check if it's a file
-            os.remove(file_path)
-            print(f'Deleted {file_path}')
+    for root, dirs, files in os.walk(data_dir):
+        for file in files:
+            if file == "NERA_TRAINING_DATA_INDEX_FILE.json":
+                with open(os.path.join(root, file), 'r') as f:
+                    data = json.load(f)
+                for file_path in data:
+                    try:
+                        os.remove(file_path)
+                        deleted_files.append(file_path)
+                    except FileNotFoundError:
+                        errors.append(f"File not found: {file_path}")
+                    except Exception as e:
+                        errors.append(f"Error deleting file: {file_path}")
+                        errors.append(e)
+
+                # Delete the index file
+                os.remove(os.path.join(root, file))
+
+    #STATUS MESSAGE
+    print("\n" + HORIZONTAL_LINE)
+    print(f"Deleted {len(deleted_files)} files from {data_dir}")
+    if errors:
+        print(f"Errors:")
+        for error in errors:
+            print(error)
+    for file in deleted_files:
+        print(file)
+    print(HORIZONTAL_LINE + "\n")
+
 
 
 
@@ -27,7 +60,8 @@ def clear_training_data(data_dir):
 # EXAMPLE USAGE
 #------------------------------------------------------------
 if __name__ == "__main__":
-    clear_training_data()
-    print("\nData cleared.\n")
+    proj_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+    data_dir = os.path.join(proj_root, 'data')
+    clear_training_data(data_dir)
 #------------------------------------------------------------
 
