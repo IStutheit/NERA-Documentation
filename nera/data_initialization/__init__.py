a70bf27739e0364dad01fcfcc3da59405f20329b
IStutheit <ImAgonizzy@gmail.com>
2024-04-09 22:57:45 -0500

    Fixed File References for GitHub Pages

--- nera/data_initialization/__init__.py
+++ nera/data_initialization/__init__.py
@@ -3,13 +3,14 @@
 
 ---
 
-## **[Download Data Files](http://localhost:8080/CSCI4970-MC-GamePlayingBot/nera/data_initialization/download_data_files.html)**
+## **[Download Data Files](https://istutheit.github.io/NERA-Documentation/nera/data_initialization/download_data_files.html)**
+
 > Grabs the urls from the contractor data to be downloaded and stored.
 
-## **[Clear Training Data](http://localhost:8080/CSCI4970-MC-GamePlayingBot/nera/data_initialization/clear_training_data.html)**
+## **[Clear Training Data](https://istutheit.github.io/NERA-Documentation/nera/data_initialization/clear_training_data.html)**
 > Deletes all files in the specified directory.
 
-## **[Process Video Frames](http://localhost:8080/CSCI4970-MC-GamePlayingBot/nera/data_initialization/process_video_frames.html)**
+## **[Process Video Frames](https://istutheit.github.io/NERA-Documentation/nera/data_initialization/process_video_frames.html)**
 > Takes in multiple files in parallel and transforms the frames into grayscale images.
 
 
