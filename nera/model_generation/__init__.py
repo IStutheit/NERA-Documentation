a307695db4530f35ae05448f007ae43c9a86db99
IStutheit <ImAgonizzy@gmail.com>
2024-05-11 14:59:22 -0500

    Added a link to view the config file

--- nera/model_generation/__init__.py
+++ nera/model_generation/__init__.py
@@ -6,14 +6,11 @@
 ## **[Neat_Run](https://istutheit.github.io/NERA-Documentation/nera/model_generation/neat_run.html)**
 > The driver class for running the NEAT algorithm
 
-## **Config**
+## **[Config](https://raw.githubusercontent.com/IStutheit/NERA-Documentation/main/nera/model_generation/config)**
 > A text file that contains all the vital NEAT setup information.
 
 ## **winner.pkl**
 > The pickle file that contains the winning neural network from the most recent neat run.
-
-
-
 
 
 """
