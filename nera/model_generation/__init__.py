e94c1d147112e3fa8a3abbfaa2cd66219a2049aa
IStutheit <ImAgonizzy@gmail.com>
2024-05-11 14:51:16 -0500

    Creation of model_generation landing page

---
+++ nera/model_generation/__init__.py
@@ -0,0 +1,19 @@
+"""
+# *The model generation directory holds the files related to running the NEAT algorithm*
+
+---
+
+## **[Neat_Run](https://istutheit.github.io/NERA-Documentation/nera/model_generation/neat_run.html)**
+> The driver class for running the NEAT algorithm
+
+## **Config**
+> A text file that contains all the vital NEAT setup information.
+
+## **winner.pkl**
+> The pickle file that contains the winning neural network from the most recent neat run.
+
+
+
+
+
+"""
